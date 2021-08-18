f82ec4f194b407feb4f3a482ec47b5df0dfb3600
choipureum <pooreumsunny@gmail.com>
2021-08-18 13:05:18 +0900

    [Refactoring] socket.io
    Signed-off-by: choipureum <pooreumsunny@gmail.com>

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,36 +1,21 @@
-const messageList = document.querySelector("ul");
-const nickForm = document.querySelector("#nick");
-const messageForm = document.querySelector("#message");
-const socket = new WebSocket(`ws://${window.location.host}`); /*window.location에는 여러 주소값존재 다음은 localhost:3000 */
+/**socket.io.use*/
+const socket = io(); 
 
-function makeMessage(type,payload)
-{
-    const msg = {type,payload};
-    return JSON.stringify(msg);
-}
+const welcome = document.getElementById("welcome");
+const form = welcome.querySelector("form");
+const room = document.getElementById("room");
+room.hidden = true;
+let roomName;
 
-socket.addEventListener("open",()=>{
-    console.log("Connected to Server");
-});
-
-socket.addEventListener("message",(message)=>{
-    const li = document.createElement("li");
-    li.innerText = message.data;
-    messageList.append(li);
-});
-
-socket.addEventListener("close",()=>{
-    console.log("Disconnected from server");
-});
-
-messageForm.addEventListener("submit",(event)=>{
-    event.preventDefault();   
-    const input = messageForm.querySelector("input");
-    socket.send(makeMessage("newMessage",input.value));
+form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    const input = form.querySelector("input");
+    socket.emit("enter_room",input.value,()=>{
+        welcome.hidden = true;
+        room.hidden = false;
+        const h3 = room.querySelector("h3");
+        h3.innerText = `Room ${roomName}`;
+    });   
+    roomName = input.value;
     input.value="";
-});
-nickForm.addEventListener("submit",(event)=>{
-    event.preventDefault();
-    const input = nickForm.querySelector("input");
-    socket.send(makeMessage("nickname",input.value));
 });
--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import SocketIO from "socket.io";
 import WebSocket from "ws";
 import http from "http";
 import { parse } from "path";
@@ -8,15 +9,28 @@ app.set("view engine","pug");
 app.set("views",__dirname + "/views");
 app.use("/public",express.static(__dirname+"/public"));
 app.get("/",(_,res) => res.render("home"));
-app.get("/*",(_,res) => res.redirect("/"));
 
 const handleListen = () => console.log("Listening on http://localhost:3000")
 
 /*app.listen(3000);*/
 /*웹소켓 서버 + http 서버 둘다 사용 가능하게 합친다.(굳이 http프로토콜 서버가 필요없으면 안해도댐)*/ 
-const server = http.createServer(app);
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);
+/**socket.io방식*/
+wsServer.on("connection",(socket)=>{
+    socket.onAny((e)=>{
+        console.log(`Socket room: ${e}`);
+    });
+    socket.on("enter_room",(roomName,done)=>{
+        socket.join(roomName);
+        done();
+    });  
+});
+
+
+/**websocket 방식 */
+/*
 const wss = new WebSocket.Server({server});
-/*접속자*/
 const sockets = [];
 
 wss.on(`connection`,(socket)=>{
@@ -37,5 +51,5 @@ wss.on(`connection`,(socket)=>{
         }      
     });
 })
-
-server.listen(3000, handleListen);
+*/
+httpServer.listen(3000, handleListen);
